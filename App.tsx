9c0376e2e43a054ca7fe5870e50e42ec9d010d37
miichu <mythng94@gmail.com>
2022-11-04 16:27:55 +0100

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import * as React from 'react';
+import { useState } from 'react';
+import AboutUs from './AboutUs';
+import FrontPage from './FrontPage';
 import './style.css';
 
+type TPage = 'home' | 'aboutUs';
+
 export default function App() {
+  const [page, setPage] = useState<TPage>('home');
+
   // hallo
 
-  /* */
+  /* 
+    Erstatt button toggling med lenker hvis det går uten å måtte legge til routing
+
+
+     <nav>
+        <a href="./FrontPage.tsx">Home</a>
+        <a href="./AboutUs.tsx">About us</a>
+      </nav>
+  */
+
   return (
     <div>
       <div className="header">
         <h1>Hello StackBlitz!</h1>
       </div>
-      <p>Start editing to see some magic happen :)</p>
-      @dsjk.cdsfdvfdfkj
-      <nav>
-        <a href="./FrontPage">Home</a>
-        <a href="./">About us</a>
-      </nav>
+
+      {page === 'home' ? <FrontPage /> : <AboutUs />}
     </div>
   );
 }
