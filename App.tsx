3a401c6ec4af84328a05c9b3a42378a1cc2e96dc
miichu <mythng94@gmail.com>
2022-11-04 16:48:23 +0100

    Added Buttons for navigation of 'pages'

--- App.tsx
+++ App.tsx
@@ -3,18 +3,16 @@ import { useState } from 'react';
 import AboutUs from './AboutUs';
 import FrontPage from './FrontPage';
 import './style.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 type TPage = 'home' | 'aboutUs';
 
 export default function App() {
   const [page, setPage] = useState<TPage>('home');
 
-  // hallo
-
   /* 
     Erstatt button toggling med lenker hvis det går uten å måtte legge til routing
-
-
      <nav>
         <a href="./FrontPage.tsx">Home</a>
         <a href="./AboutUs.tsx">About us</a>
@@ -26,6 +24,14 @@ export default function App() {
       <div className="header">
         <h1>Hello StackBlitz!</h1>
       </div>
+
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button onClick={() => setPage('home')}>Home</Button>
+        <Button onClick={() => setPage('aboutUs')}>About us</Button>
+      </ButtonGroup>
 
       {page === 'home' ? <FrontPage /> : <AboutUs />}
     </div>
