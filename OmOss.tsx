43231170cc23cbe22f1562b51d829f11f98acde8
marhaal <38426172+marhaal@users.noreply.github.com>
2022-11-11 15:22:16 +0100

    La till oppgavetekst

--- OmOss.tsx
+++ OmOss.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 
 const OmOss = () => {
+  // Oppgave 1
   // Skriv inn gruppenummer der det står Om gruppe:
 
+  // Oppgave 2
   // Kopier linje 11 og lim den inn under for hvert medlem i gruppa. Erstatt alle steder hvor det står "Navn" med navnene på jentene i gruppa.
+
+  // Eksta oppgave
+  // For å se en litt finere liste, legg til dette: className="about" etter der det står <main
+
   return (
-    <div>
-      <main id="about">
-        <h1 id="about-title">Om gruppe: </h1>
-        <div id="list-container">
+    <div className="about-container">
+      <main className="about">
+        <h1>Om gruppe: </h1>
+        <div>
           <ul>
             <li>Navn</li>
             <li>Navn</li>
